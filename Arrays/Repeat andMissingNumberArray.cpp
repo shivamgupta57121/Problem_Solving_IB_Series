35fa4312c3c4d1713fdd54e4c7caebe52a7c0c06
SHIVAM GUPTA <31137037+shivamgupta57121@users.noreply.github.com>
2020-05-21 18:06:07 +0530

    Updated Repeat and Missing Number Array

--- Arrays/Repeat andMissingNumberArray.cpp
+++ Arrays/Repeat andMissingNumberArray.cpp
@@ -22,3 +22,26 @@ vector<int> Solution::repeatedNumber(const vector<int> &A) {
     
     return ans;
 }
+
+
+// Space Complexity : O(1) 
+// Applying Mathematics
+
+vector<int> Solution::repeatedNumber(const vector<int> &A) {
+    
+    long long n = A.size();
+    vector<int> ans(2);
+    long long sumAct = 0, sumSqAct = 0;
+    long long sum = (n*(n+1))/2, sumSq = (n*(n+1)*(2*n+1))/6;
+    
+    for(int i=0;i<n;i++) {
+        sumAct+=A[i];
+        sumSqAct+=(long long)A[i]*(long long)A[i];
+    }
+    int A_Sub_B = sumAct-sum;
+    int A_Add_B = (sumSqAct-sumSq)/A_Sub_B;
+    ans[0] = (int)(A_Add_B + A_Sub_B)/2;
+    ans[1] = (int)(A_Add_B - A_Sub_B)/2;
+    
+    return ans;
+}
